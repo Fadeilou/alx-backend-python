338eef4acc12a47b773c6031f66e2aa4d3ad3940
Fadel <afadeilou@gmail.com>
2025-06-14 13:06:31 +0100

    chat2

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -10,6 +10,7 @@ class User(AbstractUser):
     first_name = models.CharField(max_length=50, blank=False)
     last_name = models.CharField(max_length=50, blank=False)
     email = models.EmailField(unique=True, blank=False)
+    password = models.CharField(max_length=128)  # Explicitly define password field
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     date_of_birth = models.DateField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
